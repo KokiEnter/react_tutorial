d948789cbfccfe43ef9557985cfe55e8bff4a51e
Xu Boo <xuboo@MacBook-Air-cua-TRAN.local>
2020-05-24 21:02:25 +0900

    refactoring: change from 3 line to 5 line

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -27,16 +27,36 @@ function Square(props) {
             {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
             {this.renderSquare(3)}
             {this.renderSquare(4)}
-            {this.renderSquare(5)}
           </div>
           <div className="board-row">
+            {this.renderSquare(5)}
             {this.renderSquare(6)}
             {this.renderSquare(7)}
             {this.renderSquare(8)}
+            {this.renderSquare(9)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(10)}
+            {this.renderSquare(11)}
+            {this.renderSquare(12)}
+            {this.renderSquare(13)}
+            {this.renderSquare(14)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(15)}
+            {this.renderSquare(16)}
+            {this.renderSquare(17)}
+            {this.renderSquare(18)}
+            {this.renderSquare(19)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(20)}
+            {this.renderSquare(21)}
+            {this.renderSquare(22)}
+            {this.renderSquare(23)}
+            {this.renderSquare(24)}
           </div>
         </div>
       );
@@ -49,7 +69,7 @@ function Square(props) {
       this.state = {
         history: [
           {
-            squares: Array(9).fill(null)
+            squares: Array(25).fill(null)
           }
         ],
         stepNumber: 0,
@@ -129,18 +149,22 @@ function Square(props) {
   
   function calculateWinner(squares) {
     const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
+      [0, 1, 2, 3, 4],
+      [5, 6, 7, 8, 9],
+      [10, 11, 12, 13, 14],
+      [15, 16, 17, 18, 19],
+      [20, 21, 22, 23, 24],
+      [0, 5, 10, 15, 20],
+      [1, 6, 11, 16, 21],
+      [2, 7, 12, 17, 22],
+      [3, 8, 13, 18, 23],
+      [4, 9, 14, 19, 24],
+      [0, 6, 12, 18, 24],
+      [4, 8, 12, 16, 20]
     ];
     for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      const [a, b, c, d, e] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d] && squares[a] === squares[e]) {
         return squares[a];
       }
     }
